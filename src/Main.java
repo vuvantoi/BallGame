8def2c959a32b9568c7cd9e71c0b5ffb3e92c9ed
vuvantoi <vuvantoi2k4@gmail.com>
2025-11-02 21:23:24 +0700

    finish vachambong

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Billiard Simulation - Step 1");
+        JFrame frame = new JFrame("Billiard Simulation - Step 3 (collision only)");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
         frame.add(new BilliardPanel());
